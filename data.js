3cf394c97c08d6d6045ad38bcfe1e7f58b19afd6
Hansamali-28 <uthpalahansamali2002@gmail.com>
2026-07-03 13:23:54 +0530

    add frst commit

---
+++ data.js
@@ -0,0 +1,15 @@
+const users = [
+  { 
+    id: 1, 
+    name: "Deeghayu Adhikari", 
+    role: "Admin" 
+  },
+  { 
+    id: 2, 
+    name: "Student", 
+    role: "User" 
+  }
+];
+
+// This line allows other files to use this data (Code Reuse!)
+module.exports = users;
